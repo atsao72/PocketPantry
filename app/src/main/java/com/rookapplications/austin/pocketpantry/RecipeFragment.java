473a9281325abba064976daa1da77473ee50ef29
atsao72 <atsao72@gmail.com>
2015-07-27 22:24:10 -0500

    Revert "Revert "Remove unnecessary factory functions""
    This reverts commit 23f668aec0f1c11a255a55e86e3c46005d81cc07.

--- app/src/main/java/com/rookapplications/austin/pocketpantry/RecipeFragment.java
+++ app/src/main/java/com/rookapplications/austin/pocketpantry/RecipeFragment.java
@@ -18,34 +18,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class RecipeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecipeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecipeFragment newInstance(String param1, String param2) {
-        RecipeFragment fragment = new RecipeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     public RecipeFragment() {
         // Required empty public constructor
@@ -54,10 +28,6 @@ public class RecipeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
