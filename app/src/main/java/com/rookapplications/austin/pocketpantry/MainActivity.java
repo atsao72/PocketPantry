68d8e0f7fb063d69433ed165265e692c29a63aca
atsao72 <atsao72@gmail.com>
2015-07-27 22:00:17 -0500

    Change titles of pages on drawer

--- app/src/main/java/com/rookapplications/austin/pocketpantry/MainActivity.java
+++ app/src/main/java/com/rookapplications/austin/pocketpantry/MainActivity.java
@@ -59,13 +59,10 @@ public class MainActivity extends ActionBarActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = "My Pantry";
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
+                mTitle = "Find Recipes";
                 break;
         }
     }
